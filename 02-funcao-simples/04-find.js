1ea752d2f48d22f1ce1d7cb98609ececad73b416
ISABELA VITORIA OLIVEIRA HOLANDESI BORGES <26171930@edu119.local>
2026-08-18 14:04:07 -0300

    Adicionando métodos

---
+++ 02-funcao-simples/04-find.js
@@ -0,0 +1,9 @@
+const alunos = ["Isabela", "Julia", "Larissa", "Sofia"];
+
+const aluno = alunos.find((nome) => nome === "Isabela");
+
+console.log(alunos);
+
+const aluno1 = alunos.find((nome) => nome === "Felipe");
+
+console.log(aluno1);
