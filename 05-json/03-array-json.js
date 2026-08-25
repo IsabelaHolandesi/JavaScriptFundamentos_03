ee8a940a29016ae1f52a4923f5636126af750a1b
ISABELA VITORIA OLIVEIRA HOLANDESI BORGES <26171930@edu119.local>
2026-08-25 11:48:26 -0300

    Atualizando

---
+++ 05-json/03-array-json.js
@@ -0,0 +1,13 @@
+const alunos = [
+    { nome: "Ana", nota: 9},
+    { nome: "Alice", nota: 7},
+];
+
+alunos.forEach((a) => console.log(`A aluna ${a.nome} a nota: ${a.nota}`));
+
+for (let i = 0; i < alunos.length; i++) {
+    console.log(`A alunos ${alunos[i].nome} a nota: ${alunos[i].nota}`)
+}
+
+const json = JSON.stringify(alunos);
+console.log(json);
--- 07-poo/01-encapsulamento.js
+++ 07-poo/01-encapsulamento.js
@@ -1,7 +1,8 @@
 class ContaBancaria {
     #saldo = 0;
+
     depositar (valor){
-        if(valor>0) this.#saldo += valor;
+        if(valor >0) this.#saldo += valor;
     }
 
     consultarSaldo(){
@@ -11,4 +12,4 @@ class ContaBancaria {
 
 const contaTiago = new ContaBancaria();
 contaTiago.depositar(100);
-console.log(contaTiago.consultarSaldo);
+console.log(contaTiago.consultarSaldo());
